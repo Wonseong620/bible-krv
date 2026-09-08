58bb35935c7ae5dcc32fb87194cae67710202bb1
Wonseong620 <kindws@gmail.com>
2026-09-08 11:22:43 +0200

    Reconnect public counseling gateway

--- counsel-config.js
+++ counsel-config.js
@@ -3,5 +3,5 @@
 window.COUNSEL_CONFIG = {
   endpoint: ['127.0.0.1', 'localhost'].includes(location.hostname)
     ? '/api/chat'
-    : 'https://silver-slight-reaction-know.trycloudflare.com/api/chat',
+    : 'https://assigned-mariah-circumstances-operator.trycloudflare.com/api/chat',
 };
